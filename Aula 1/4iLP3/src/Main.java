c9ed6ebacf22ba3d0ef1111c0b2a9c8a8ca7f194
Gabriel Dias Henz <gabrield47@gmail.com>
2017-02-24 11:16:17 -0300

    fix(Executavel): alterar nome da classe e adicionar pessoas.

--- Aula 1/4iLP3/src/Main.java
+++ Aula 1/4iLP3/src/Main.java
@@ -2,16 +2,18 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class Main {
+public class Executavel {
 
 	public static void main(String[] args) {
 		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("4iLP3");
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		
-		Pessoa pessoa = new Pessoa("Lucas");
+		Pessoa lucas = new Pessoa("Lucas Kauer");
+		Pessoa gabriel = new Pessoa("Gabriel Henz");
 		
 		entityManager.getTransaction().begin();
-		entityManager.persist(pessoa);
+		entityManager.persist(lucas);
+		entityManager.persist(gabriel);
 		entityManager.getTransaction().commit();
 		
 		entityManager.close();
